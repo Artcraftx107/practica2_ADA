27a0788eca8012837f85bafc6d028db4df97640d
Artcraft <arturoao@uma.es>
2024-10-10 19:13:34 +0200

    Added possible solution for exercise 3

--- src/main/java/ArraysConRepeticiones.java
+++ src/main/java/ArraysConRepeticiones.java
@@ -7,5 +7,25 @@ public class ArraysConRepeticiones {
 	
 	private static int encuentraElem(int [] v, int izq, int der) {
 		//Completar la implementación del método
+		int elem = -1;
+		while (izq<=der){
+			int mitad = izq+(der-izq)/2;
+
+			//Devolver elemento repetido si lo encontramos (solo hay uno repetido n veces)
+			if(mitad>0 && v[mitad]==v[mitad-1]){
+				elem = v[mitad];
+			} else {
+				if (mitad < v.length - 1 && v[mitad] == v[mitad + 1]) {
+					elem = v[mitad];
+				}
+			}
+
+			if(v[mitad]-mitad>v[0]){
+				der=mitad-1;
+			}else{
+				izq=mitad+1;
+			}
+		}
+		return elem; 
 	}
 }
